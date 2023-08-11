48f9f6dbfb69165d8066a95969b19385a7848144
Sanjeev Bharti <sanjeev.bharti@nirvanahealth.com>
2023-08-11 16:18:28 -0400

    Pushed checkmark code

---
+++ Sources/Checkmark/Checkmark.swift
@@ -0,0 +1,7 @@
+public struct Checkmark {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+        
+    }
+}
---
+++ Sources/Checkmark/CheckmarkButtonView.swift
@@ -0,0 +1,51 @@
+//
+//  CheckmarkButtonView.swift
+//  ButtonUI
+//
+//  Created by Sanjeev Bharti on 7/28/23.
+//
+
+import SwiftUI
+
+public enum FillModeEnum {
+    case empty
+    case fill
+}
+
+@available(iOS 13.0, *)
+public struct CheckmarkButtonView: View {
+    
+    @Binding public var isSelected: Bool
+    
+    public var foregroundColor: Color
+    public var fillMode: FillModeEnum
+    public var maxWidth: CGFloat
+    
+    private var fillImageName: String
+    private var emptyImageName: String
+    
+    init(isSelected: Binding<Bool>, foregroundColor: Color = .primary, fillMode: FillModeEnum = .empty, maxWidth: CGFloat = 40.0) {
+        _isSelected = isSelected
+        self.foregroundColor = foregroundColor
+        self.fillMode = fillMode
+        self.maxWidth = maxWidth
+        emptyImageName = fillMode == .empty ? "square" : "square.fill"
+        fillImageName = fillMode == .empty ? "checkmark.square" : "checkmark.square.fill"
+    }
+    
+    public var body: some View {
+        
+        Button {
+            isSelected.toggle()
+        } label: {
+        
+            Image(systemName: isSelected ? fillImageName : emptyImageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(foregroundColor)
+                .frame(width: maxWidth)
+                .animation(.spring(response: 0.3, dampingFraction: 0.7, blendDuration: 0.1), value: isSelected)
+        }
+
+    }
+}
